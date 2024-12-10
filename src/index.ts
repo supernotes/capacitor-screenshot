43a2477a941c83735c7a65c9b47291bd01c7a928
tobeagram <tobiaswhetton@hotmail.com>
2024-12-10 18:22:35 +0000

    Fix module resolution issues by specifying file extensions

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
+import type { ScreenshotPlugin } from './definitions';
 import { registerPlugin } from '@capacitor/core';
 
-import type { ScreenshotPlugin } from './definitions';
-
 const Screenshot = registerPlugin<ScreenshotPlugin>('Screenshot', {
-  web: () => import('./web').then(m => new m.ScreenshotWeb()),
+  web: () => import('./web.js').then(m => new m.ScreenshotWeb()),
 });
 
-export * from './definitions';
+export * from './definitions.js';
 export { Screenshot };
